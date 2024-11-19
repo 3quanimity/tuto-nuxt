48d3d7d206447a0c72a2055840c703b2b69e1371
Haythem <sinnner6@gmail.com>
2024-11-19 12:08:20 +0100

    more routing

--- pages/blog/article-[id].vue
+++ pages/blog/article-[id].vue
@@ -1,4 +1,13 @@
 <template>
-    <h1>Article 1</h1>
+    <h1>Article {{ id }}</h1>
+    <p>Contenu de l'article {{ id }}</p>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus dicta blanditiis, quaerat, molestiae quidem, deserunt voluptatem quisquam deleniti sed iste molestias distinctio! Error quia autem, aut dolorum accusamus exercitationem nesciunt.</p>
+    <NuxtLink to="/blog">Retour au blog</NuxtLink>
 </template>
+
+<script lang="ts" setup>
+    const route = useRoute()
+    const id = route.params.id
+    // fetch API
+</script>
+
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,3 +1,7 @@
 <template>
     <h1>List des Articles</h1>
+
+    <ul>
+        <li v-for="i in 5"><NuxtLink :to="'/blog/article-'+i">Article {{ i }}</NuxtLink></li>
+    </ul>
 </template>
